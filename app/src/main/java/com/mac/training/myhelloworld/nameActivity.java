ad50eca91906442438d4cbe4a85124b76bfe2400
Jaime Santiesteban <jaimesantiesteban07@gmail.com>
2016-08-17 18:50:43 -0400

    Hello world

---
+++ app/src/main/java/com/mac/training/myhelloworld/nameActivity.java
@@ -0,0 +1,13 @@
+package com.mac.training.myhelloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
